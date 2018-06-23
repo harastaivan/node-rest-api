a71bc33d6549eabd9ad72a1f1fbaf4fa63d15dd5
Ivan Harasta <harasta.ivan@gmail.com>
2018-06-23 19:26:33 +0200

    npm install mongoose and connect to mongoDB Atlas
    credentials are in file nodemon.json which is in .gitignore

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://' + process.env.MONGO_ATLAS_USERNAME + ':' + process.env.MONGO_ATLAS_PW + '@cluster0-if0dy.mongodb.net/test?retryWrites=true');
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({
